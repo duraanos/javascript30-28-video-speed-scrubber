cdc9a978e5906851f46fa4d93d374e12608f1b74
duraanos <duran364@anadolu.edu.tr>
2024-08-16 14:52:58 +0300

    Code javascript file

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const speed = document.querySelector('.speed');
+const speedBar = speed.querySelector('.speed-bar');
+
+const video = document.querySelector('.flex');
+
+speed.addEventListener('mousemove', function (e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const min = 0.4;
+  const max = 4;
+  const height = Math.round(percent * 100) + '%';
+  const playbackRate = percent * (max - min) + min;
+  speedBar.style.height = height;
+  speedBar.textContent = playbackRate.toFixed(2) + 'x';
+  video.playbackRate = playbackRate
+});
